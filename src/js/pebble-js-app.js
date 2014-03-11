fe0b6b2c75aea79653c0e8942ef1a6314fd0efa8
Rogier Slag <rogierslag@users.noreply.github.com>
2014-03-11 07:56:45 -0700

    Shows the current duration of a task
    Todo:
    - Stop the current timer
    - Start a new timer

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,11 +1,22 @@
+var id = localStorage.getItem("lastId");
+//var token = localStorage.getItem("token");
+var token = "e7d2ad36d546dd60702083029c03a9b6";
+
 function send(token,method,call,json) {
+	token = 'e7d2ad36d546dd60702083029c03a9b6';
     var xhr = new XMLHttpRequest();
-    var auth = btoa(token+':api_token');
+    var auth = "Basic "+base64.encode(token+':api_token');
+	console.log(auth);
     xhr.open(method, "https://toggl.com/api/v8/"+call, false);
-    xhr.setRequestHeader('Authorization',auth,'Content-type','application/json');
-    xhr.setRequestHeader("Content-length", json.length);
+    xhr.setRequestHeader('Authorization',auth);
+	xhr.setRequestHeader('Content-type','application/json');
+	if ( json != null ) {
+		xhr.setRequestHeader("Content-length", json.length);
+	}
     xhr.send(json);
-    return JSON.parse(xhr.responseText);
+	console.log(xhr.status);
+	console.log(xhr.responseText);
+	return JSON.parse(xhr.responseText);
 }
 
 function startTimer() {
@@ -19,7 +30,7 @@ function startTimer() {
 
 function stopTimer(id) {
     var result = send(token,"PUT","time_entries/"+id+"/stop",null);
-    return result.data.duration;
+    return result.data;
 }
 
 function getTimer() {
@@ -28,22 +39,290 @@ function getTimer() {
 }
 
 Pebble.addEventListener("ready",
-  function(e) {
-    console.log("JavaScript app ready and running!");
-  }
-);
+                        function(e) {
+                            console.log("JavaScript app ready and running!");
+                            var data = getTimer();
+							console.log(data);
+
+							if ( data ) {
+								Pebble.sendAppMessage({
+									"id": data.id,
+									"duration": data.duration,
+									"description": data.description,
+									"start":"1"
+								});
+							} else {
+								Pebble.sendAppMessage({
+									"id": 0,
+									"duration": 0,
+									"description": "",
+									"start":"0"
+								});
+							}
+                            
+                        }
+                        );
 
 Pebble.addEventListener("appmessage",
-  function(e) {
-    console.log("Received message: " + e.payload);
-	  if (e.type == 'start') {
-		  id = startTimer()
-	  } else if (e.type == 'stop' ) {
-		  stopTimer(id);
-	  }
-  }
-);
+                        function(e) {
+                            console.log("Received message: " + e.payload);
+                            if (e.type == 'start') {
+                                id = startTimer()
+                            } else if (e.type == 'stop' ) {
+                                stopTimer(id);
+                            }
+                        }
+                        );
 
-var id = localStorage.getItem("lastId");
-//var token = localStorage.getItem("token");
-var token = "e7d2ad36d546dd60702083029c03a9b6";
+				  
+				  /** LIBRARY **/
+				  
+// UTF8 Module
+//
+// Cleaner and modularized utf-8 encoding and decoding library for javascript.
+//
+// copyright: MIT
+// author: Nijiko Yonskai, @nijikokun, nijikokun@gmail.com
+(function (name, definition, context, dependencies) {
+  if (typeof context['module'] !== 'undefined' && context['module']['exports']) { if (dependencies && context['require']) { for (var i = 0; i < dependencies.length; i++) context[dependencies[i]] = context['require'](dependencies[i]); } context['module']['exports'] = definition.apply(context); }
+  else if (typeof context['define'] !== 'undefined' && context['define'] === 'function' && context['define']['amd']) { define(name, (dependencies || []), definition); }
+  else { context[name] = definition.apply(context); }
+})('utf8', function () {
+  return {
+    encode: function (string) {
+      if (typeof string !== 'string') return string;
+      else string = string.replace(/\r\n/g, "\n");
+      var output = "", i = 0, charCode;
+ 
+      for (i; i < string.length; i++) {
+        charCode = string.charCodeAt(i);
+ 
+        if (charCode < 128)
+          output += String.fromCharCode(charCode);
+        else if ((charCode > 127) && (charCode < 2048))
+          output += String.fromCharCode((charCode >> 6) | 192),
+          output += String.fromCharCode((charCode & 63) | 128);
+        else
+          output += String.fromCharCode((charCode >> 12) | 224),
+          output += String.fromCharCode(((charCode >> 6) & 63) | 128),
+          output += String.fromCharCode((charCode & 63) | 128);
+      }
+ 
+      return output;
+    },
+ 
+    decode: function (string) {
+      if (typeof string !== 'string') return string;
+      var output = "", i = 0, charCode = 0;
+ 
+      while (i < string.length) {
+        charCode = string.charCodeAt(i);
+ 
+        if (charCode < 128)
+          output += String.fromCharCode(charCode),
+          i++;
+        else if ((charCode > 191) && (charCode < 224))
+          output += String.fromCharCode(((charCode & 31) << 6) | (string.charCodeAt(i + 1) & 63)),
+          i += 2;
+        else
+          output += String.fromCharCode(((charCode & 15) << 12) | ((string.charCodeAt(i + 1) & 63) << 6) | (string.charCodeAt(i + 2) & 63)),
+          i += 3;
+      }
+ 
+      return output;
+    }
+  };
+}, this);
+ 
+// Base64 Module
+//
+// Cleaner, modularized and properly scoped base64 encoding and decoding module for strings.
+//
+// copyright: MIT
+// author: Nijiko Yonskai, @nijikokun, nijikokun@gmail.com
+(function (name, definition, context, dependencies) {
+  if (typeof context['module'] !== 'undefined' && context['module']['exports']) { if (dependencies && context['require']) { for (var i = 0; i < dependencies.length; i++) context[dependencies[i]] = context['require'](dependencies[i]); } context['module']['exports'] = definition.apply(context); }
+  else if (typeof context['define'] !== 'undefined' && context['define'] === 'function' && context['define']['amd']) { define(name, (dependencies || []), definition); }
+  else { context[name] = definition(); }
+})('base64', function (utf8) {
+  var $this = this;
+  var $utf8 = utf8 || this.utf8;
+  var map = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+ 
+  return {
+    encode: function (input) {
+      if (typeof $utf8 === 'undefined') throw { error: "MissingMethod", message: "UTF8 Module is missing." };
+      if (typeof input !== 'string') return input;
+      else input = $utf8.encode(input);
+      if (typeof $this.btoa !== 'undefined') return $this.btoa(input);
+      var output = "", a, b, c, d, e, f, g, i = 0;
+ 
+      while (i < input.length) {
+        a = input.charCodeAt(i++);
+        b = input.charCodeAt(i++);
+        c = input.charCodeAt(i++);
+        d = a >> 2;
+        e = ((a & 3) << 4) | (b >> 4);
+        f = ((b & 15) << 2) | (c >> 6);
+        g = c & 63;
+ 
+        if (isNaN(b)) f = g = 64;
+        else if (isNaN(c)) g = 64;
+ 
+        output += map.charAt(d) + map.charAt(e) + map.charAt(f) + map.charAt(g);
+      }
+ 
+      return output;
+    },
+ 
+    decode: function (input) {
+      if (typeof $utf8 === 'undefined') throw { error: "MissingMethod", message: "UTF8 Module is missing." };
+      if (typeof input !== 'string') return input;
+      else input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+      if (typeof $this.atob !== 'undefined') return $this.atob(input);
+      var output = "", a, b, c, d, e, f, g, i = 0;
+ 
+      while (i < input.length) {
+        d = map.indexOf(input.charAt(i++));
+        e = map.indexOf(input.charAt(i++));
+        f = map.indexOf(input.charAt(i++));
+        g = map.indexOf(input.charAt(i++));
+ 
+        a = (d << 2) | (e >> 4);
+        b = ((e & 15) << 4) | (f >> 2);
+        c = ((f & 3) << 6) | g;
+ 
+        output += String.fromCharCode(a);
+        if (f != 64) output += String.fromCharCode(b);
+        if (g != 64) output += String.fromCharCode(c);
+      }
+ 
+      return $utf8.decode(output);
+    }
+  }
+}, this, [ "utf8" ]);
+																															   
+																															   // Base64 Module
+//
+// Cleaner, modularized and properly scoped base64 encoding and decoding module for strings.
+//
+// copyright: MIT
+// author: Nijiko Yonskai, @nijikokun, nijikokun@gmail.com
+(function (name, definition, context, dependencies) {
+  if (typeof context['module'] !== 'undefined' && context['module']['exports']) { if (dependencies && context['require']) { for (var i = 0; i < dependencies.length; i++) context[dependencies[i]] = context['require'](dependencies[i]); } context['module']['exports'] = definition.apply(context); }
+  else if (typeof context['define'] !== 'undefined' && context['define'] === 'function' && context['define']['amd']) { define(name, (dependencies || []), definition); }
+  else { context[name] = definition(); }
+})('base64', function (utf8) {
+  var $this = this;
+  var $utf8 = utf8 || this.utf8;
+  var map = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+ 
+  return {
+    encode: function (input) {
+      if (typeof $utf8 === 'undefined') throw { error: "MissingMethod", message: "UTF8 Module is missing." };
+      if (typeof input !== 'string') return input;
+      else input = $utf8.encode(input);
+      if (typeof $this.btoa !== 'undefined') return $this.btoa(input);
+      var output = "", a, b, c, d, e, f, g, i = 0;
+ 
+      while (i < input.length) {
+        a = input.charCodeAt(i++);
+        b = input.charCodeAt(i++);
+        c = input.charCodeAt(i++);
+        d = a >> 2;
+        e = ((a & 3) << 4) | (b >> 4);
+        f = ((b & 15) << 2) | (c >> 6);
+        g = c & 63;
+ 
+        if (isNaN(b)) f = g = 64;
+        else if (isNaN(c)) g = 64;
+ 
+        output += map.charAt(d) + map.charAt(e) + map.charAt(f) + map.charAt(g);
+      }
+ 
+      return output;
+    },
+ 
+    decode: function (input) {
+      if (typeof $utf8 === 'undefined') throw { error: "MissingMethod", message: "UTF8 Module is missing." };
+      if (typeof input !== 'string') return input;
+      else input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+      if (typeof $this.atob !== 'undefined') return $this.atob(input);
+      var output = "", a, b, c, d, e, f, g, i = 0;
+ 
+      while (i < input.length) {
+        d = map.indexOf(input.charAt(i++));
+        e = map.indexOf(input.charAt(i++));
+        f = map.indexOf(input.charAt(i++));
+        g = map.indexOf(input.charAt(i++));
+ 
+        a = (d << 2) | (e >> 4);
+        b = ((e & 15) << 4) | (f >> 2);
+        c = ((f & 3) << 6) | g;
+ 
+        output += String.fromCharCode(a);
+        if (f != 64) output += String.fromCharCode(b);
+        if (g != 64) output += String.fromCharCode(c);
+      }
+ 
+      return $utf8.decode(output);
+    }
+  }
+}, this, [ "utf8" ]);
+																															   
+																															   // UTF8 Module
+//
+// Cleaner and modularized utf-8 encoding and decoding library for javascript.
+//
+// copyright: MIT
+// author: Nijiko Yonskai, @nijikokun, nijikokun@gmail.com
+(function (name, definition, context, dependencies) {
+  if (typeof context['module'] !== 'undefined' && context['module']['exports']) { if (dependencies && context['require']) { for (var i = 0; i < dependencies.length; i++) context[dependencies[i]] = context['require'](dependencies[i]); } context['module']['exports'] = definition.apply(context); }
+  else if (typeof context['define'] !== 'undefined' && context['define'] === 'function' && context['define']['amd']) { define(name, (dependencies || []), definition); }
+  else { context[name] = definition.apply(context); }
+})('utf8', function () {
+  return {
+    encode: function (string) {
+      if (typeof string !== 'string') return string;
+      else string = string.replace(/\r\n/g, "\n");
+      var output = "", i = 0, charCode;
+ 
+      for (i; i < string.length; i++) {
+        charCode = string.charCodeAt(i);
+ 
+        if (charCode < 128)
+          output += String.fromCharCode(charCode);
+        else if ((charCode > 127) && (charCode < 2048))
+          output += String.fromCharCode((charCode >> 6) | 192),
+          output += String.fromCharCode((charCode & 63) | 128);
+        else
+          output += String.fromCharCode((charCode >> 12) | 224),
+          output += String.fromCharCode(((charCode >> 6) & 63) | 128),
+          output += String.fromCharCode((charCode & 63) | 128);
+      }
+ 
+      return output;
+    },
+ 
+    decode: function (string) {
+      if (typeof string !== 'string') return string;
+      var output = "", i = 0, charCode = 0;
+ 
+      while (i < string.length) {
+        charCode = string.charCodeAt(i);
+ 
+        if (charCode < 128)
+          output += String.fromCharCode(charCode),
+          i++;
+        else if ((charCode > 191) && (charCode < 224))
+          output += String.fromCharCode(((charCode & 31) << 6) | (string.charCodeAt(i + 1) & 63)),
+          i += 2;
+        else
+          output += String.fromCharCode(((charCode & 15) << 12) | ((string.charCodeAt(i + 1) & 63) << 6) | (string.charCodeAt(i + 2) & 63)),
+          i += 3;
+      }
+ 
+      return output;
+    }
+  };
+}, this);
--- src/main.c
+++ src/main.c
@@ -5,16 +5,59 @@ TextLayer *content_layer;
 TextLayer *title_layer;
 ActionBarLayer* action_bar;
 
+uint32_t GcurrentTid;
+uint32_t Gduration;
+char *Gdescription;
+
 static const int TITLE_HEIGHT=50;
+static const int TIME_ZONE_OFFSET=1;
 
 GBitmap *icon_start;
 GBitmap *icon_stop;
+
+enum keys {
+	APPMESS_start,
+	APPMESS_stop,
+	APPMESS_get,
+	APPMESS_id,
+	APPMESS_duration,
+	APPMESS_description
+};
+
+
+char* calculateDuration(struct tm *current_time, int duration) {
+	unsigned int unix_time;
+    unsigned int running_time;
+    /* Convert time to seconds since epoch. */
+    unix_time = ((0-TIME_ZONE_OFFSET)*3600) + /* time zone offset */ 
+              + current_time->tm_sec /* start with seconds */
+              + current_time->tm_min*60 /* add minutes */
+              + current_time->tm_hour*3600 /* add hours */
+              + current_time->tm_yday*86400 /* add days */
+              + (current_time->tm_year-70)*31536000 /* add years since 1970 */
+              + ((current_time->tm_year-69)/4)*86400 /* add a day after leap years, starting in 1973 */
+              - ((current_time->tm_year-1)/100)*86400 /* remove a leap day every 100 years, starting in 2001 */
+              + ((current_time->tm_year+299)/400)*86400; /* add a leap day back every 400 years, starting in 2001*/
+
+    running_time = unix_time+duration;
+
+	static char buf[64] = "";
+	APP_LOG(APP_LOG_LEVEL_INFO, "unix time %u",unix_time);
+	APP_LOG(APP_LOG_LEVEL_INFO, "calculated %u",running_time);
+	
+	snprintf(buf, sizeof(buf), "%u:%02u:%02u", running_time/(60*60), (running_time/60)%60, running_time%60);
+	return buf;
+}
 
 void start() {
 	
 }
 
 void stop(int id) {
+	
+}
+
+void get() {
 	
 }
 
@@ -34,7 +77,7 @@ static void click_config_provider(void *context) {
 void my_window_load(Window *window) {
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_frame(window_layer);
-	GRect content_layer_bounds = GRect(bounds.origin.x,bounds.origin.y+TITLE_HEIGHT,bounds.size.w-ACTION_BAR_WIDTH-10,bounds.size.h-TITLE_HEIGHT);
+	GRect content_layer_bounds = GRect(3,bounds.origin.y+TITLE_HEIGHT,bounds.size.w-ACTION_BAR_WIDTH-10-3,bounds.size.h-TITLE_HEIGHT);
 	
 	title_layer = text_layer_create(GRect(0,0,bounds.size.w-ACTION_BAR_WIDTH-10,TITLE_HEIGHT));
 	text_layer_set_background_color(title_layer, GColorWhite);
@@ -47,7 +90,7 @@ void my_window_load(Window *window) {
 	text_layer_set_background_color(content_layer, GColorWhite);
 	text_layer_set_text_color(content_layer, GColorBlack);
 	text_layer_set_text_alignment(content_layer, GTextAlignmentCenter);
-	text_layer_set_font(content_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+	text_layer_set_font(content_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
 	text_layer_set_text(content_layer, "time to Toggl!");
 	
 	icon_start = gbitmap_create_with_resource (RESOURCE_ID_ICON_START_BLACK);
@@ -56,16 +99,36 @@ void my_window_load(Window *window) {
 	action_bar = action_bar_layer_create();
   	// Associate the action bar with the window:
  	action_bar_layer_set_click_config_provider(action_bar, (ClickConfigProvider) click_config_provider);
-	action_bar_layer_set_icon(action_bar, BUTTON_ID_UP, icon_start);
-	action_bar_layer_set_icon(action_bar, BUTTON_ID_DOWN, icon_stop);
 	
 	action_bar_layer_add_to_window(action_bar, window);
  	layer_add_child(window_layer, text_layer_get_layer(title_layer));
 	layer_add_child(window_layer, text_layer_get_layer(content_layer));
-	
+}
+
+void updateCurrentTimer(struct tm *tick_time, TimeUnits units_changed) {
+	static char buf[64] = "";
+
+	snprintf(buf, sizeof(buf), "A timer for %s is running for %s", Gdescription, calculateDuration(tick_time,Gduration));
+    text_layer_set_text(content_layer, buf);
+
+	APP_LOG(APP_LOG_LEVEL_INFO, buf);
+}
+
+
+void updateSysCurrentTimer(Tuple *id, Tuple *duration, Tuple *description) {
 	// Discover the current timer and display the duration and description
 	// If no timer is present, offer the possibility to start a new one
 	// Other wise offer the possibility to stop the current timer
+	int idValue = id->value->uint32;
+	int durationValue = duration->value->uint32;
+	char *descValue = description->value->cstring;
+	
+	GcurrentTid = idValue;
+	Gduration = durationValue;
+	Gdescription = descValue;
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Id is %d and duration is %d",idValue,durationValue);
+	
+	tick_timer_service_subscribe(SECOND_UNIT ,updateCurrentTimer);
 }
 
 void my_window_unload(Window *window) {
@@ -78,6 +141,39 @@ void handle_deinit(void) {
 	  window_destroy(my_window);
 }
 
+void in_received_handler(DictionaryIterator *it, void *context) {
+	APP_LOG(APP_LOG_LEVEL_INFO, "Goodie I received somethingy!");	
+	Tuple *currentTimer = dict_find(it,APPMESS_start);
+	if ( currentTimer) {
+		if ( strcmp(currentTimer->value->cstring,"1") == 0)  {
+			APP_LOG(APP_LOG_LEVEL_INFO, "Goodie I received a running timer");	
+			Tuple *id = dict_find(it,APPMESS_id);
+			Tuple *duration = dict_find(it,APPMESS_duration);
+			Tuple *description = dict_find(it,APPMESS_description);
+			updateSysCurrentTimer(id,duration,description);
+			action_bar_layer_set_icon(action_bar, BUTTON_ID_DOWN, icon_stop);
+		} else {
+			APP_LOG(APP_LOG_LEVEL_INFO, "Goodie I received not a timer at all!");
+			text_layer_set_text(content_layer, "No timer is currently running");
+			action_bar_layer_set_icon(action_bar, BUTTON_ID_UP, icon_start);
+		}
+	}
+		
+}
+
+void in_dropped_handler(AppMessageResult reason, void *context) {
+	APP_LOG(APP_LOG_LEVEL_WARNING, "Something was received incorrectly");
+}
+
+void out_sent_handler(DictionaryIterator *it, void *context) {
+	APP_LOG(APP_LOG_LEVEL_INFO, "I sent somethingy to theh interwebs");
+}
+
+void out_failed_handler(DictionaryIterator *it, AppMessageResult reasion, void *context) {
+	APP_LOG(APP_LOG_LEVEL_WARNING, "Something was sent incorrectly");
+}
+
+
 void handle_init(void) {
 	my_window = window_create();
 
@@ -86,6 +182,13 @@ void handle_init(void) {
 		.unload = my_window_unload,
 	});
 
+	app_message_register_inbox_received(in_received_handler);
+	app_message_register_inbox_dropped(in_dropped_handler);
+	app_message_register_outbox_sent(out_sent_handler);
+	app_message_register_outbox_failed(out_failed_handler);
+	
+	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+    
 	window_stack_push(my_window, true);
 }
 
